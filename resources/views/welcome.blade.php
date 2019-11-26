6e8bdb980b7dd10e07fca3d4c16e2834baafabc2
bto-2019 <58235092+bto-2019@users.noreply.github.com>
2019-11-26 17:43:30 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Proyecto Integrador con Sensores Sistemas-Mecánica
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Informacion General</a>
+                    <a href="https://laracasts.com">Arduino</a>
+                    <a href="https://laravel-news.com">Datos</a>
+                    <a href="https://blog.laravel.com">Graficas</a>
                 </div>
             </div>
         </div>
